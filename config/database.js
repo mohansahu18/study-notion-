d752fbad1851cdf42d6ff84ca3fcf3dc2a56f5c6
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-21 19:05:17 +0530

    setup the db connection

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const DB_URL = process.env.DATABASE_URL;
+
+const dbconnection = () => {
+
+    mongoose.connect(DB_URL, {
+    }).then(() => console.log("successfully connected to db"))
+        .catch((err) => {
+            console.log("issue with db connection");
+            console.log(err);
+        })
+}
+module.exports = dbconnection
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require('dotenv').config();
-
+const dbconnection = require("./config/database")
 
 const PORT = process.env.PORT || 3000
 
@@ -11,3 +11,5 @@ app.use(express.json())
 app.listen(PORT, () => {
     console.log(`server started on port no. ${PORT}`);
 })
+
+dbconnection()
