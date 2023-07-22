46a13e94217b3f5965fc7a594d33468acefcc880
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-22 11:12:47 +0530

    completed course progress model

---
+++ models/courseProgress.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const courseProgressSchema = new mongoose.Schema({
+
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+    },
+    completedVideos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubSection"
+        }
+    ]
+})
+
+module.exports = mongoose.model("CourseProgress", courseProgressSchema)
