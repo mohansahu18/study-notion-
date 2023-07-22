be809de691fdb7fcbff1e40033ea97c5552140b0
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-22 13:09:02 +0530

    setup nodemailer

---
+++ utils/mailSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer")
+require("dotenv").config()
+
+const mailSender = async (email, title, body) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            secure: true,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+        });
+
+        const info = await transporter.sendMail({
+            from: "STUDY NOTION",
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`,
+        })
+        console.log(`info : - > ${info}`);
+        return info
+
+    } catch (err) {
+        console.log("unable to send otp");
+        console.log(err);
+    }
+}
+
+module.exports = mailSender
