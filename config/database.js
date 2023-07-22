32835c448b0dfb837304b5210be24c15b95f63c4
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-22 11:05:00 +0530

    completed profile model

--- config/database.js
+++ config/database.js
@@ -6,6 +6,8 @@ const DB_URL = process.env.DATABASE_URL;
 const dbconnection = () => {
 
     mongoose.connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }).then(() => console.log("successfully connected to db"))
         .catch((err) => {
             console.log("issue with db connection");
