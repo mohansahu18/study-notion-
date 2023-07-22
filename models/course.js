32ddcec3ed08ae04e000ca48a6deee5c4ee53991
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-22 12:01:49 +0530

    completed course model

---
+++ models/course.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+
+const courseSchema = new mongoose.Schema({
+
+    courseName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    courseDescription: {
+        type: String,
+        trim: true
+    },
+    instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    whatYouWillLearn: {
+        type: String,
+        trim: true
+    },
+    courseContent: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section"
+        }
+    ],
+    ratingAndReview: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RatingAndReview"
+        }
+    ],
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    thumbnail: {
+        type: String
+    },
+    tags: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tags"
+    },
+    studentEnroll: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    ]
+})
+
+module.exports = mongoose.model("Course", courseSchema)
