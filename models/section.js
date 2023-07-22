54827af033d92b09c1fb98111204668f46ba4bba
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-22 11:39:57 +0530

    completed section model

---
+++ models/section.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const sectionSchema = new mongoose.Schema({
+    sectionName: {
+        type: String
+    },
+    subSection: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubSection",
+            required: true
+        }
+    ]
+})
+
+module.exports = mongoose.model("Section", sectionSchema)
