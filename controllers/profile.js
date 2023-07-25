502863e647b497bc6e1937ee64fb3464bca65e1c
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-26 01:02:19 +0530

    completed delete acount  handler in profile controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -44,4 +44,42 @@ const updateProfile = async (req, res) => {
     }
 }
 
-module.exports = { updateProfile }
+const deleteAccount = async (req, res) => {
+    try {
+        // fetch data
+        const { userId } = req.user.id
+        const userDetail = User.findById(userId)
+        // validate
+        if (!userDetail) {
+            return res.status(404).json({
+                success: false,
+                message: "user not found"
+            })
+        }
+
+        // delete profile of of the user
+        const profileId = userDetail.additionalDetails
+        const deletedProfile = await Profile.findByIdAndDelete({ profileId })
+        // delete user
+        const deletedUser = await User.findByIdAndDelete({
+            _id: userId
+        })
+
+        // return success response
+        return res.status(200).json({
+            success: true,
+            message: "user deleted successfully",
+            deletedUser: deletedUser
+        })
+
+    } catch (err) {
+        console.log("error in deleting a profile ", err);
+        return res.status(500).json({
+            success: false,
+            message: "not able to delete profile",
+            error: err.message
+        })
+
+    }
+}
+module.exports = { updateProfile, deleteAccount }
