106db1562f837723f9f838da39f6d8130a4d14f3
mohan lal sahu <mohansahu18111811@gmail.com>
2023-08-07 12:48:48 +0530

    error page completed

--- frontend/src/component/pages/ErrorPage.js
+++ frontend/src/component/pages/ErrorPage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useRouteError } from "react-router-dom";
+import SOMETHING_WRONG from "../../assets/Images/someth-went-wrong.png"
 
 const Error = () => {
+    const err = useRouteError();
+    const { status, statusText } = err
+    console.log(err);
     return (
-        <div>
-            error page
+        <div className="flex text-white flex-col justify-center items-center flex-wrap">
+            <h3 className=" font-medium text-2xl text-red-500 pt-5">  {status + ":" + statusText} </h3>
+            <div className="flex flex-col justify-center flex-wrap" >
+                <img src={SOMETHING_WRONG} />
+            </div>
         </div>
     )
-}
 
-export default Error
+}
+export default Error;
